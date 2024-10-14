75e48cbd5db4d2ebecc77b3b6668212a0b0389f4
MichelleL <michelleloi885@gmail.com>
2024-10-14 03:06:18 -0600

    CHORE: changed modal button style

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ### Images:
 - Bomb images from: https://sketchfab.com/3d-models/c4-bomb-fc33f6537918437c80ceeb0d51707e50
+- Num Pad: https://sketchfab.com/3d-models/keypad-b9968a00bc9249df927ff1969acf2f7e
--- src/components/bomb/Bomb.jsx
+++ src/components/bomb/Bomb.jsx
@@ -6,6 +6,12 @@ import bombTop from '../../assets/bomb_top.png';
 import bombBottom from '../../assets/bomb_bottom.png';
 import bombSide from '../../assets/bomb_side.png';
 import Light from '../light/Light.jsx';
+import key1 from '../../assets/key_1.png';
+import key2 from '../../assets/key_2.png';
+import key3 from '../../assets/key_3.png';
+import key4 from '../../assets/key_4.png';
+import key5 from '../../assets/key_5.png';
+import key6 from '../../assets/key_6.png';
 
 /**
  * This code is based on: https://3dtransforms.desandro.com/box
@@ -19,42 +25,42 @@ const Bomb = ({ side, setPuzzleNum }) => {
           <div className="bomb__face bomb__face--front">
             <img src={bombFront} alt="" className="bomb__img" />
             <button className="bomb__modal-btn" onClick={() => setPuzzleNum(1)}>
-              Puzzle 1
+              <img className="modal-btn__img" src={key1} alt="" />
             </button>
             <Light />
           </div>
           <div className="bomb__face bomb__face--back">
             <img src={bombBack} alt="" className="bomb__img" />
             <button className="bomb__modal-btn" onClick={() => setPuzzleNum(2)}>
-              Puzzle 2
+              <img className="modal-btn__img" src={key2} alt="" />
             </button>
             <Light />
           </div>
           <div className="bomb__face bomb__face--right">
             <img src={bombSide} alt="" className="bomb__img" />
             <button className="bomb__modal-btn" onClick={() => setPuzzleNum(3)}>
-              Puzzle 3
+              <img className="modal-btn__img" src={key3} alt="" />
             </button>
             <Light />
           </div>
           <div className="bomb__face bomb__face--left">
             <img src={bombSide} alt="" className="bomb__img" />
             <button className="bomb__modal-btn" onClick={() => setPuzzleNum(4)}>
-              Puzzle 4
+              <img className="modal-btn__img" src={key4} alt="" />
             </button>
             <Light />
           </div>
           <div className="bomb__face bomb__face--top">
             <img src={bombTop} alt="" className="bomb__img" />
             <button className="bomb__modal-btn" onClick={() => setPuzzleNum(5)}>
-              Puzzle 5
+              <img className="modal-btn__img" src={key5} alt="" />
             </button>
             <Light />
           </div>
           <div className="bomb__face bomb__face--bottom">
             <img src={bombBottom} alt="" className="bomb__img" />
             <button className="bomb__modal-btn" onClick={() => setPuzzleNum(6)}>
-              Puzzle 6
+              <img className="modal-btn__img" src={key6} alt="" />
             </button>
             <Light />
           </div>
