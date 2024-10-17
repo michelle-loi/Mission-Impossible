e1c96f56555f99bc98dc29cf44e2d544cee80d93
MichelleL <michelleloi885@gmail.com>
2024-10-17 03:38:41 -0600

    CHORE: remove unnecessary styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import { useState } from 'react';
 import Toggles from './components/toggles/Toggles.jsx';
 import Modal from './components/modal/Modal.jsx';
 import LightSensor from './components/battery/LightSensor.jsx';
-import Battery from './components/battery/Battery.jsx';
-import circuitBG from './assets/circuit.png';
 
 function App() {
   const [side, setSide] = useState('front');
@@ -14,20 +12,16 @@ function App() {
     setPuzzleNum(-1);
   };
 
-  const batteryModalStyles = {
-    background: `url(${circuitBG}) no-repeat center center`,
-    backgroundSize: 'cover',
-  };
-
   return (
     <div className="app-container">
       <Bomb side={side} setPuzzleNum={setPuzzleNum} />
       <Toggles side={side} setSide={setSide} />
       {puzzleNum === 1 && (
-        <Modal closeModal={clearPuzzle} styles={batteryModalStyles}>
+        <Modal closeModal={clearPuzzle}>
           <LightSensor />
         </Modal>
       )}
+
       {puzzleNum === 2 && <Modal closeModal={clearPuzzle} />}
       {puzzleNum === 3 && <Modal closeModal={clearPuzzle} />}
       {puzzleNum === 4 && <Modal closeModal={clearPuzzle} />}
