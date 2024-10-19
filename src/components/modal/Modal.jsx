aceab140cd24409716e354a5c725e73957a7c9fe
MichelleL <michelleloi885@gmail.com>
2024-10-19 02:22:55 -0600

    FEAT: add animation for modal opening

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,13 +1,44 @@
 import './Modal.scss';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useEffect } from 'react';
 
-const Modal = ({ children, closeModal }) => {
+const Modal = ({ children, closeModal, isVisible = false }) => {
+  // handles modal animation
+  const listVariants = {
+    closed: {
+      opacity: 0,
+      transition: {
+        ease: 'easeInOut',
+        duration: 0.5,
+      },
+    },
+    open: {
+      opacity: 1,
+      transition: {
+        ease: 'easeInOut',
+        duration: 0.5,
+      },
+    },
+  };
+
   return (
-    <div className="modal-container">
-      <button className="modal__set-btn" onClick={closeModal}>
-        Set
-      </button>
-      {children}
-    </div>
+    <AnimatePresence>
+      {isVisible && (
+        <motion.div
+          className="modal-container"
+          variants={listVariants}
+          initial="closed"
+          animate="open"
+          exit="closed"
+          key={'modal-framer-motion-key'}
+        >
+          <button className="modal__set-btn" onClick={closeModal}>
+            Set
+          </button>
+          {children}
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
