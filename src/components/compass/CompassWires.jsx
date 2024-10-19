3ef6be0f4dc69565654cd01949add13c3a3f3d13
MichelleL <michelleloi885@gmail.com>
2024-10-18 21:37:46 -0600

    CHORE: change divs to map

--- src/components/compass/CompassWires.jsx
+++ src/components/compass/CompassWires.jsx
@@ -43,77 +43,19 @@ const CompassWires = () => {
   return (
     <div className="compass-wires-container">
       <div className="compass-wires">
-        <div className="wire wire1">
-          <div className="light-drop-container">
-            <div className="light-drop light-drop-1"></div>
+        {[1, 2, 3, 4, 5, 6].map((wireNum) => (
+          <div className={`wire wire${wireNum}`} key={`WIRE-UNIQUE-${wireNum}`}>
+            <div className="light-drop-container">
+              <div className={`light-drop light-drop-${wireNum}`}></div>
+            </div>
+            <img
+              className={`resistor resistor-${wireNum} ${pendingResistor === `res-${wireNum}` ? 'resistor-overheat' : ''}`}
+              src={resistorIMG}
+              alt=""
+              onClick={() => handleResistorClick(`res-${wireNum}`)}
+            />
           </div>
-          <img
-            className={`resistor resistor-1 ${pendingResistor === 'res-1' ? 'resistor-overheat' : ''}`}
-            src={resistorIMG}
-            alt=""
-            onClick={() => handleResistorClick('res-1')}
-          />
-        </div>
-
-        <div className="wire wire2">
-          <div className="light-drop-container">
-            <div className="light-drop light-drop-2"></div>
-          </div>
-          <img
-            className={`resistor resistor-2 ${pendingResistor === 'res-2' ? 'resistor-overheat' : ''}`}
-            src={resistorIMG}
-            alt=""
-            onClick={() => handleResistorClick('res-2')}
-          />
-        </div>
-
-        <div className="wire wire3">
-          <div className="light-drop-container">
-            <div className="light-drop light-drop-3"></div>
-          </div>
-          <img
-            className={`resistor resistor-3 ${pendingResistor === 'res-3' ? 'resistor-overheat' : ''}`}
-            src={resistorIMG}
-            alt=""
-            onClick={() => handleResistorClick('res-3')}
-          />
-        </div>
-
-        <div className="wire wire4">
-          <div className="light-drop-container">
-            <div className="light-drop light-drop-4"></div>
-          </div>
-          <img
-            className={`resistor resistor-4 ${pendingResistor === 'res-4' ? 'resistor-overheat' : ''}`}
-            src={resistorIMG}
-            alt=""
-            onClick={() => handleResistorClick('res-4')}
-          />
-        </div>
-
-        <div className="wire wire5">
-          <div className="light-drop-container">
-            <div className="light-drop light-drop-5"></div>
-          </div>
-          <img
-            className={`resistor resistor-5 ${pendingResistor === 'res-5' ? 'resistor-overheat' : ''}`}
-            src={resistorIMG}
-            alt=""
-            onClick={() => handleResistorClick('res-5')}
-          />
-        </div>
-
-        <div className="wire wire6">
-          <div className="light-drop-container">
-            <div className="light-drop light-drop-6"></div>
-          </div>
-          <img
-            className={`resistor resistor-6 ${pendingResistor === 'res-6' ? 'resistor-overheat' : ''}`}
-            src={resistorIMG}
-            alt=""
-            onClick={() => handleResistorClick('res-6')}
-          />
-        </div>
+        ))}
       </div>
       <div className="compass-wires-compass">
         <Compass heading={heading} setHeading={setHeading} />
