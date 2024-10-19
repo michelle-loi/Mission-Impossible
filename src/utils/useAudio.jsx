8d4e1e3a3493974b20843ed72622d083e25007bd
MichelleL <michelleloi885@gmail.com>
2024-10-19 02:41:44 -0600

    CHORE: add default volume

--- src/utils/useAudio.jsx
+++ src/utils/useAudio.jsx
@@ -1,4 +1,4 @@
-export function playAudio(audio, volume, startTime = 0) {
+export function playAudio(audio, volume = 1, startTime = 0) {
   audio.volume = volume;
   audio.currentTime = startTime;
   audio.play();
