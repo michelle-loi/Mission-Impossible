2ea4dcf8c86690b19b035e2c9ecf3bb8ce408873
MichelleL <michelleloi885@gmail.com>
2024-10-21 22:56:54 -0600

    FEAT: implement home screen

--- src/pages/homescreen/HomeScreen.jsx
+++ src/pages/homescreen/HomeScreen.jsx
@@ -1,5 +1,45 @@
+import './HomeScreen.scss';
+import gmail from '../../assets/new-logo-gmail-svgrepo-com.png';
+import maps from '../../assets/google-logo-new-maps-svgrepo-com.png';
+import photos from '../../assets/google-logo-photos-new-svgrepo-com.png';
+import youtube from '../../assets/youtube-svgrepo-com.png';
+import chrome from '../../assets/chrome-svgrepo-com.png';
+import google from '../../assets/google-logo-search-new-svgrepo-com.png';
+import assistant from '../../assets/assistant-logo-new-google-svgrepo-com.png';
+import Clock from '../../components/clock/Clock.jsx';
+import {
+  MdBattery90,
+  MdNetworkWifi,
+  MdSignalCellular3Bar,
+} from 'react-icons/md';
 const HomeScreen = () => {
-  return <div>Homescreen</div>;
+  return (
+    <div className="homescreen">
+      <div className="homescreen__upper">
+        <div className="homescreen__info">
+          <MdNetworkWifi />
+          <MdSignalCellular3Bar />
+          <MdBattery90 />
+        </div>
+        <div>
+          <Clock />
+        </div>
+      </div>
+      <div className="homescreen__lower">
+        <div className="homescreen__apps">
+          <img className="homescreen__logo" src={gmail} alt="" />
+          <img className="homescreen__logo" src={maps} alt="" />
+          <img className="homescreen__logo" src={photos} alt="" />
+          <img className="homescreen__logo" src={youtube} alt="" />
+          <img className="homescreen__logo" src={chrome} alt="" />
+        </div>
+        <div className="homescreen__search">
+          <img className="search-logo" src={google} alt="" />
+          <img className="search-logo" src={assistant} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomeScreen;
--- src/pages/lockscreen/LockScreen.jsx
+++ src/pages/lockscreen/LockScreen.jsx
@@ -6,46 +6,10 @@ import { MdSignalCellular3Bar } from 'react-icons/md';
 import { MdBattery90 } from 'react-icons/md';
 import { FaSun } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
+import Clock from '../../components/clock/Clock.jsx';
 
 const LockScreen = () => {
   const navigate = useNavigate();
-
-  const [dateTime, setDateTime] = useState({
-    date: '',
-    time: '',
-  });
-
-  useEffect(() => {
-    const updateDateTime = () => {
-      const now = new Date();
-
-      // Format date with NA locale
-      const date = now.toLocaleDateString('en-US', {
-        weekday: 'long',
-        month: 'long',
-        day: 'numeric',
-      });
-
-      // Format 12-hour clock with no seconds
-      let time = now.toLocaleTimeString('en-US', {
-        hour: 'numeric',
-        minute: 'numeric',
-        hour12: true,
-      });
-
-      time = time.replace(/ AM| PM/, '');
-
-      setDateTime({ date, time });
-    };
-
-    // Call function on mount to set date and time
-    updateDateTime();
-
-    // Call function every second to update
-    const intervalId = setInterval(updateDateTime, 1000);
-
-    return () => clearInterval(intervalId);
-  }, []);
 
   // https://stackoverflow.com/questions/70612769/how-do-i-recognize-swipe-events-in-react
   const [touchStart, setTouchStart] = useState(null);
@@ -83,11 +47,7 @@ const LockScreen = () => {
         <MdBattery90 />
       </div>
       <div className="lockscreen__datetime">
-        <div className="lockscreen__time">{dateTime.time}</div>
-        <div className="lockscreen__date">
-          {dateTime.date}
-          <FaSun />
-        </div>
+        <Clock />
       </div>
 
       <div className="lockscreen__camera">
